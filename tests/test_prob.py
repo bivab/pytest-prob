8ee64a0f8fd2eca276d1be40627f4cbb525ba0dc
David Schneider <david.schneider@bivab.de>
2015-11-05 16:47:00 +0100

    simple testcases

--- tests/test_prob.py
+++ tests/test_prob.py
@@ -1,64 +1,39 @@
 # -*- coding: utf-8 -*-
 
-
-def test_bar_fixture(testdir):
-    """Make sure that pytest accepts our fixture."""
-
-    # create a temporary pytest test module
-    testdir.makepyfile("""
-        def test_sth(bar):
-            assert bar == "europython2015"
+def test_simple_predicate(testdir):
+    testdir.makeconftest('pytest_plugins = "prob"')
+    testdir.makefile('.yml', test_truth="""
+        test_predicate:
+            test: "1=1"
     """)
-
-    # run pytest with the following cmd args
-    result = testdir.runpytest(
-        '--foo=europython2015',
-        '-v'
-    )
-
-    # fnmatch_lines does an assertion internally
-    result.stdout.fnmatch_lines([
-        '*::test_sth PASSED',
-    ])
-
-    # make sure that that we get a '0' exit code for the testsuite
-    assert result.ret == 0
-
-
-def test_help_message(testdir):
-    result = testdir.runpytest(
-        '--help',
-    )
-    # fnmatch_lines does an assertion internally
-    result.stdout.fnmatch_lines([
-        'prob:',
-        '*--foo=DEST_FOO*Set the value for the fixture "bar".',
-    ])
-
-
-def test_hello_ini_setting(testdir):
-    testdir.makeini("""
-        [pytest]
-        HELLO = world
-    """)
-
-    testdir.makepyfile("""
-        import pytest
-
-        @pytest.fixture
-        def hello(request):
-            return request.config.getini('HELLO')
-
-        def test_hello_world(hello):
-            assert hello == 'world'
-    """)
-
     result = testdir.runpytest('-v')
 
-    # fnmatch_lines does an assertion internally
     result.stdout.fnmatch_lines([
-        '*::test_hello_world PASSED',
+        '*::test_predicate PASSED',
     ])
 
-    # make sure that that we get a '0' exit code for the testsuite
-    assert result.ret == 0
+
+def test_simple_false_predicate(testdir):
+    testdir.makeconftest('pytest_plugins = "prob"')
+    testdir.makefile('.yml', test_truth="""
+        test_predicate:
+            test: "1 /= 1"
+    """)
+    result = testdir.runpytest('-v')
+
+    result.stdout.fnmatch_lines([
+        '*::test_predicate FAILED',
+    ])
+
+
+def test_truth(testdir):
+    testdir.makeconftest('pytest_plugins = "prob"')
+    testdir.makefile('.yml', test_truth="""
+        test_truth:
+            test: "TRUE"
+    """)
+    result = testdir.runpytest('-v')
+
+    result.stdout.fnmatch_lines([
+        '*::test_truth PASSED',
+    ])
