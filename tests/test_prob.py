61a824d17d8bac6693e53558637f79f1a9a33a57
David Schneider <david.schneider@bivab.de>
2015-11-06 08:30:16 +0100

    Added test for missing probcli binary in PATH

--- tests/test_prob.py
+++ tests/test_prob.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 
 def test_simple_predicate(testdir):
     testdir.makeconftest('pytest_plugins = "prob"')
@@ -25,6 +26,23 @@ def test_simple_false_predicate(testdir):
         '*::test_predicate FAILED',
     ])
 
+
+def test_missing_prob(testdir):
+    path = os.environ['PATH']
+    os.environ['PATH'] = ''
+    try:
+        testdir.makeconftest('pytest_plugins = "prob"')
+        testdir.makefile('.yml', test_truth="""
+            test_predicate:
+                test: "1=1"
+        """)
+        result = testdir.runpytest('-v')
+
+        result.stdout.fnmatch_lines([
+            '*probcli not found in PATH',
+        ])
+    finally:
+        os.environ['PATH'] = path
 
 def test_truth(testdir):
     testdir.makeconftest('pytest_plugins = "prob"')
