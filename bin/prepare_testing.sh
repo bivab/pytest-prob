dc90b7cdc5ca1db34a72466c8d3f8660883e80a1
David Schneider <david.schneider@bivab.de>
2015-11-13 08:33:29 +0100

    Download and unpack ProB for testing

---
+++ bin/prepare_testing.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+os=`uname`
+
+case $os in
+
+Darwin)
+    echo "Running on OS X"
+    url="http://nightly.cobra.cs.uni-duesseldorf.de/tcl/ProB.mac_os.10.7.5.x86_64.tar.gz"
+    ;;
+
+Linux)
+    echo "Running on Linux"
+    wordsize=`uname -m`
+    if [ $wordsize = "x86_64" ]
+    then
+      echo "64 bit"
+      url="http://nightly.cobra.cs.uni-duesseldorf.de/tcl/ProB.linux64.tar.gz"
+    else
+      echo "32 bit"
+      url="http://nightly.cobra.cs.uni-duesseldorf.de/tcl/ProB.linux32.tar.gz"
+    fi
+    ;;
+
+*)
+    >&2 echo "Failure: Unsupported OS '${os}'"
+    exit 1
+    ;;
+esac
+
+curl ${url} -z download.tar.gz > download.tar.gz
+mkdir prob
+tar -xvf download.tar.gz -C prob --strip-components 1
+rm -rf prob
